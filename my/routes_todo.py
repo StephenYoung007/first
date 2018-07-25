af5c6ac433e9541cddd777e7749f99e5b71df2d8
wy1309374767@gmail.com <wy1314520>
2018-07-26 00:00:38 +0800

    为todo页面添加编辑按钮

--- my/routes_todo.py
+++ my/routes_todo.py
@@ -61,8 +61,46 @@ def delete_todo(request):
     return redirect('/todo')
 
 
+def edit(request):
+    headers = {
+        'Content-Type': 'text/html',
+    }
+    uname = current_user(request)
+    u = User.find_by(username=uname)
+    if u is None:
+        return redirect('/login')
+    todo_id = int(request.query.get('id', -1))
+    t = Todo.find_by(id=todo_id)
+    if t.user_id != u.id:
+        return redirect('/login')
+    body = templates('todo_edit.html')
+    body = body.replace('{{todo_id}}', str(t.id))
+    # print('t.id', t.id)
+    body = body.replace('{{todo_title}}', str(t.title))
+    header = response_with_headers(headers)
+    r = header + '\r\n' + body
+    return  r.encode(encoding='utf-8')
+
+
+def update(request):
+    uname = current_user(request)
+    u = User.find_by(username=uname)
+    if u is None:
+        return redirect('/login')
+    if request.method == 'POST':
+        form = request.form()
+        todo_id = int(form.get('id', -1))
+        t = Todo.find_by(id=todo_id)
+        t.title = form.get('title', t.title)
+        t.save()
+    return redirect('/todo')
+
+
+
 todo_routes = {
     '/todo' : index,
     '/todo/add' : add,
     '/todo/delete' : delete_todo,
+    '/todo/edit' : edit,
+    '/todo/update' : update,
 }
